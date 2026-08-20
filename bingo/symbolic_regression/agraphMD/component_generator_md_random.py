ee6e53eb6a872fddf520eba89db02d8166daf57b
JENKINS <1633673361157005@mil>
2026-08-20 13:35:12 -0400

    renamed files for clarity

--- bingo/symbolic_regression/agraphMD/component_generator_md_random.py
+++ bingo/symbolic_regression/agraphMD/component_generator_md_random.py
@@ -8,8 +8,6 @@ import logging
 import numpy as np
 
 from .operator_definitions import *
-from .validation_backend.operator_validate import validate_operator
-from .validation_backend.validation_backend import get_stack_command_dimensions
 from ...util.probability_mass_function import ProbabilityMassFunction
 from ...util.argument_validation import argument_validation
 
@@ -27,8 +25,7 @@ class ComponentGeneratorMD:
         number of commands at the beginning of stack which are required to be
         terminals. Default 1
     terminal_probability : float [0.0-1.0]
-        probability that a new node will be a terminal. Default 0.1
-    constant_probability : float [0.0-1.0] (optional)
+        probability that a new node will be a terminal. Default 0.1*    constant_probability : float [0.0-1.0] (optional)
         probability that a new terminal will be a constant
 
     Attributes
@@ -40,7 +37,6 @@ class ComponentGeneratorMD:
                          terminal_probability={">=": 0.0, "<=": 1.0},
                          constant_probability={">=": 0.0, "<=": 1.0})
     def __init__(self, input_x_dimensions=None,
-                 input_y_dimension=None,
                  possible_dims=None,
                  possible_dim_weights=None,
                  x_weights=None,
@@ -53,17 +49,12 @@ class ComponentGeneratorMD:
             input_x_dimensions = []
         input_x_dimensions = [input_dim if input_dim != () else (0, 0) for input_dim in input_x_dimensions]
         self.input_x_dimensions = np.array(input_x_dimensions)
-        self.x_dims = [tuple(row) for row in self.input_x_dimensions]
-        self.input_y_dimension = input_y_dimension
         self.possible_dims = possible_dims
         self.possible_dim_weights = possible_dim_weights
         self.x_weights = x_weights
         self.num_x = len(input_x_dimensions)
         self._num_initial_load_statements = num_initial_load_statements
 
-        # self._terminals = self.initiate_terminals(terminal_probability)
-        self._dim_stack = []
-        self._pos_operator_pmf = ProbabilityMassFunction()
         self._terminal_pmf = self._make_terminal_pdf(constant_probability)
         self._operator_pmf = ProbabilityMassFunction()
         self._random_command_function_pmf = \
@@ -109,22 +100,8 @@ class ComponentGeneratorMD:
             if operator_string in operator_names:
                 return operator_number
         raise ValueError("Could not find operator %s. " % operator_string)
-        
-    # def initiate_terminals(self, terminal_probability):
-    #     terminals = []
-    #     num_terminals = np.random.randint((self.stack_size*terminal_probability))
-    #     for i in range(self._num_initial_load_statements):
-    #         terminals.append(self.random_terminal_command(i))
-        
-    #     for i in range(self.num_initial_load_statements, num_terminals):
-    #         terminals.append(self.random_terminal_command(i))
-            
-    #     return terminals
-    
-    def possible_operators(self):
-        return self._operator_pmf.items
-        
-    def random_command(self, stack_location, dependent_stack):
+
+    def random_command(self, stack_location):
         """Get a random command
 
         Parameters
@@ -140,103 +117,7 @@ class ComponentGeneratorMD:
         """
         if stack_location < self._num_initial_load_statements:
             return self.random_terminal_command(stack_location)
-        
-        self._pos_operator_pmf = ProbabilityMassFunction()
-
-        constant_dims = []
-        for i, (node, param1, param2, param3) in enumerate(dependent_stack):
-            if node == 1:
-                constant_dims.append((param2, param3))
-        
-        stack_dimensions = get_stack_command_dimensions(dependent_stack, self.x_dims, constant_dims)
-
-        for i, dim1 in enumerate(stack_dimensions):
-            for j, dim2 in enumerate(stack_dimensions):
-                for operator in self.possible_operators():
-                    
-                    dimensions = np.array([dim1,
-                                            dim2])
-                    valid, op_dims = validate_operator(operator, 0, 1, 0, dimensions, 0, 0)
-                    if valid:
-                        self._pos_operator_pmf.add_item([operator, i, j, np.array(op_dims)])
-                    
-                    dimensions = np.array([dim2,
-                                            dim1])
-                    valid, op_dims = validate_operator(operator, 0, 1, 0, dimensions, 0, 0)
-                    if valid:
-                        self._pos_operator_pmf.add_item([operator, j, i, np.array(op_dims)])
-
-        sample = self._pos_operator_pmf.draw_sample()
-        return np.array([sample[0], sample[1], sample[2], 0])
-
-    def random_command_full(self, stack_location):
-        """Get a random command
-
-        Parameters
-        ----------
-        stack_location : int
-            location in the stack for the command
-
-        Returns
-        -------
-        array of int
-            a random command in the form [node, parameter 1, parameter 2, parameter 3]
-
-        """
-        # if stack_location < self._num_initial_load_statements:
-        #     return self.random_terminal_command(stack_location)
-        # return self._random_command_function_pmf.draw_sample()(stack_location)
-        # print(len(self._dim_stack))
-        if stack_location == 0:
-            self._dim_stack = []
-            self._pos_operator_pmf = ProbabilityMassFunction()
-        if stack_location < self._num_initial_load_statements:
-            if stack_location == 0 and self.input_y_dimension is not None:
-                command = np.array([1, 0, self.input_y_dimension[0], self.input_y_dimension[1]])
-                self._dim_stack.append(np.array(self.input_y_dimension))
-                return command
-            command = self.random_terminal_command(stack_location)
-            self._dim_stack.append(np.array([command[2], command[3]]))
-            return command
-        else:
-            random_command = self._random_command_function_pmf.draw_sample()
-            if random_command == self.random_terminal_command:
-                command = self.random_terminal_command(stack_location)
-                self._dim_stack.append(np.array([command[2], command[3]]))
-                return command
-            else:
-                for i, dim1 in enumerate(self._dim_stack):
-                    for operator in self.possible_operators():
-                        dim2 = self._dim_stack[-1]
-                        
-                        dimensions = np.array([dim1,
-                                               dim2])
-                        valid, op_dims = validate_operator(operator, 0, 1, 0, dimensions, 0, 0)
-                        if valid:
-                            self._pos_operator_pmf.add_item([operator, i, stack_location-1, np.array(op_dims)])
-                        
-                        dimensions = np.array([dim2,
-                                               dim1])
-                        valid, op_dims = validate_operator(operator, 0, 1, 0, dimensions, 0, 0)
-                        if valid:
-                            self._pos_operator_pmf.add_item([operator, stack_location-1, i, np.array(op_dims)])
-                        
-                # if stack_location == 29:
-                #     attempts = 0
-                #     sample = self._pos_operator_pmf.draw_sample()
-                #     while not np.array_equal(sample[3], [3,3]):
-                #         if attempts > 50:
-                #             return np.array([sample[0], sample[1], sample[2], 0])
-                #         sample = self._pos_operator_pmf.draw_sample()
-                #         attempts += 1
-                # else:
-                #     sample = self._pos_operator_pmf.draw_sample()
-
-                sample = self._pos_operator_pmf.draw_sample()
-                self._dim_stack.append(np.array(sample[3]))
-                return np.array([sample[0], sample[1], sample[2], 0])
-                
-                
+        return self._random_command_function_pmf.draw_sample()(stack_location)
 
     def random_operator_command(self, stack_location):
         """Get a random operator (non-terminal) command
